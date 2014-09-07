c04f62bb5b11806ed91ed90f392f5cf3e8782206
aureooms <aurelien.ooms@gmail.com>
2014-09-07 13:37:54 +0200

    stash

--- js/dist/oro.js
+++ js/dist/oro.js
@@ -117,36 +117,54 @@ exports.base = base;
  *   - all constraints are equalities
  *
  *
+ * @return {int} the number of effectively used variables
  *
  * /!\ not finished, must investigate the case where x >= 0
  */
 
-var normalize = function(c, A, m, n) {
-	var swap, i, j, Ai, Ak, k = n - 1;
+var normalize_t = function(fn){
 
-	v : for (i = 0; i <= k; ++i) {
+	var normalize = function(A, m, n) {
+		var swap, i, j, Ai, Ak, Am, k = n - 1;
 
-		Ai = A[i];
+		Am = A[m];
 
-		for (j = 0; j <= m; ++j) if (Ai[j] !== 0) continue v;
+		v : for (j = 0; j <= k; ++j) {
 
-		Ak = A[k];
+			Ai = A[i];
 
-		for (j = 0; j <= m; ++j) {
-			swap  = Ai[j];
-			Ai[j] = Ak[j];
-			Ak[j] = swap;
+			
+			if (Am[j] == 0) {
+				// if variable is not present in objective function
+				// check whether it can be used to increase an other variable
+				// in order to obtain a admissible solution 
+				// with arbitrary high value
+
+				// TODO continue
+				// for (j = 0; j < i; ++j) if(A[_i][])
+			}
+
+			for (j = 0; j <= m; ++j) if (Ai[j] !== 0) continue v;
+
+			Ak = A[k];
+
+			for (j = 0; j <= m; ++j) {
+				swap  = Ai[j];
+				Ai[j] = Ak[j];
+				Ak[j] = swap;
+			}
+
+			--i;
+			--k;
 		}
 
-		--i;
-		--k;
-	}
+		return k + 1;
 
-	return k + 1;
+	};
 
 };
 
-exports.normalize = normalize;
+exports.normalize_t = normalize_t;
 /* /home/genius/dev/oro/js/src/simplex/simplex.js */
 
 
