135f8c52ae208e365ad06c7817bcfcdc1a57ed60
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 12:34:30 +0100

    rebuild

--- js/dist/oro.js
+++ js/dist/oro.js
@@ -1,6 +1,8 @@
-(function(exports, undefined){
+( function ( ) {
 
-	'use strict';
+'use strict' ;
+
+var definition = function ( exports , undefined ) {
 
 
 /* js/src/simplex */
@@ -237,4 +239,16 @@ var simplex = function(A, m, n) {
 	}
 
 };
-})(typeof exports === 'undefined' ? this['oro'] = {} : exports);
+return exports ;
+} ;
+if ( typeof exports === "object" ) {
+	definition( exports ) ;
+}
+else if ( typeof define === "function" && define.amd ) {
+	define( "@aureooms/js-oro" , [ ] , function ( ) { return definition( { } ) ; } ) ;
+}
+else if ( typeof window === "object" && typeof window.document === "object" ) {
+	definition( window["oro"] = { } ) ;
+}
+else console.error( "unable to detect type of module to define for @aureooms/js-oro") ;
+} )( ) ;
